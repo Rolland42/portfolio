49784254577fce45e6a6846d153df4c988110b26
sonia <s.rolland@codeur.online>
2021-06-09 20:56:38 +0200

    test

---
+++ card/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Portfolio Card</title>
+	<link rel="stylesheet" href="main.css" />
+</head>
+<body>
+<article>
+      <h1>Sonia Rolland</h1>
+	  </article>
+
+
+
+
+
+<div class="card-box">
+
+	<div class="card">
+		<div class="card__inner">
+			<div class="card__face card__face--front">
+				<h2>DES</h2>
+			</div>
+			<div class="card__face card__face--back">
+				<div class="card__content">
+					<div class="card__header">
+						<img src="img/photo.jpg" alt="photo Sonia" class="pp" />
+						<h2>Hello!!</h2>
+					</div>
+                    <button class="btn-grad" type="button" >click</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__inner-2">
+			<div class="card__face card__face--front">
+				<h2>IGN</h2>
+			</div>
+			<div class="card__face card__face--back">
+				<div class="card__content">
+					<div class="card__header">
+						<img src="img/urbain.jpg" alt="logo urbain" class="pp" />
+						<h2>Création</h2>
+					</div>
+                    <button class="btn-grad" type="button" >click</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<div class="card">
+		<div class="card__inner-3">
+			<div class="card__face card__face--front">
+				<h2>ER</h2>
+			</div>
+			<div class="card__face card__face--back">
+				<div class="card__content">
+					<div class="card__header">
+						<img src="img/rond.gif" alt="logo S" class="pp" />
+						<h2>Projets</h2>
+					</div>
+                    <button class="btn-grad" type="button" >click</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__inner-4">
+			<div class="card__face card__face--front">
+				<h2>WEB</h2>
+			</div>
+			<div class="card__face card__face--back">
+				<div class="card__content">
+					<div class="card__header">
+						<img src="img/merci.svg" alt="logo merci" class="pp" />
+						<h2>Contact</h2>
+					</div>
+                    <button class="btn-grad" type="button" >click</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+	<script src="main.js"></script>
+</body>
+</html>
