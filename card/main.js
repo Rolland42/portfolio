f6ff8271b88f1c39acd574103457045b9c1b79da
sonia <s.rolland@codeur.online>
2021-06-16 16:57:11 +0200

    test

--- card/main.js
+++ card/main.js
@@ -1,24 +1,3 @@
-/* const card = document.querySelector(".card__inner");
-const card2 = document.querySelector(".card__inner-2");
-const card3 = document.querySelector(".card__inner-3");
-const card4 = document.querySelector(".card__inner-4");
-
-card.addEventListener("click", function (e) {
-  card.classList.toggle('is-flipped');
-}); 
-
-card2.addEventListener("click", function (e) {
-  card2.classList.toggle('is-flipped');
-});
-
-card3.addEventListener("click", function (e) {
-  card3.classList.toggle('is-flipped');
-});
-
-card4.addEventListener("click", function (e) {
-  card4.classList.toggle('is-flipped');
-}); 
- */
 
   
  
